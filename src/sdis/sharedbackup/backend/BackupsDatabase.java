58fec0a35f40a79105f9c22a5e9e29759dc5d7c0
Duarte Brandão <duartepbrandao@hotmail.com>
2014-03-25 10:17:10 -0700

    commit

--- src/sdis/sharedbackup/backend/BackupsDatabase.java
+++ src/sdis/sharedbackup/backend/BackupsDatabase.java
@@ -1,7 +1,6 @@
 package sdis.sharedbackup.backend;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
